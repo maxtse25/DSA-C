46ba18e26e4889f749e78a33577a1c85262d0da6
maxtse25 <tsechunghei97@outlook.com>
2024-07-28 22:14:23 +0800

    Added Selection Sort

---
+++ sort/Selection_Sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Function to perform selection sort
+void selectionSort(int a[], int n) {
+    int i, j, minIndex;
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Perform swapping between the first element with the found minimum element
+        if (minIndex != i) {
+            swap(&a[i], &a[minIndex]);
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {99, 11, 88, 33, 66, 55, 44, 77, 22};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Oringial array: \n");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
