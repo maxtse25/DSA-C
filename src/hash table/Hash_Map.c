e08e840514f3282a163ff9e21a17f254d80c0800
Max <tsechunghei97@outlook.com>
2024-08-09 20:25:10 +0800

    Updated Hash Map

--- src/hash table/Hash_Map.c
+++ src/hash table/Hash_Map.c
@@ -42,4 +42,27 @@ HashMap* createHashMap() {
 void insert(HashMap* map, char* key, int value) {
     unsigned int bucketIndex = hash(key);
     HashNode* new_node = malloc(sizeof(HashNode));
+    new_node->key = strdup(key);
+    new_node->value = value;
+    new_node->next = map->buckets[bucketIndex];
+    map->buckets[bucketIndex] = new_node;
+}
+
+// Search for a key in the hash map
+int search(HashMap* map, char* key) {
+    unsigned int bucketIndex = hash(key);
+    HashNode* node = map->buckets[bucketIndex];
+
+    while(node) {
+        if (strcmp(node->key, key) == 0) {
+            return node->value;
+        }
+        node = node->next;
+    }
+    return -1;
+}
+
+// Remove a key from the hash map
+void removeKey(HashMap* map, char* key) {
+    unsigned int bucketIndex = hash(key);
 }
