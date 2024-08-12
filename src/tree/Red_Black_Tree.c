6c28cb77ef8b20cc20d816090e762ab678cb415a
Max <tsechunghei97@outlook.com>
2024-08-12 21:16:05 +0800

    Updated Red-Black Trees

--- src/tree/Red_Black_Tree.c
+++ src/tree/Red_Black_Tree.c
@@ -1,3 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { RED, BLACK } Color;
+
+typedef struct Node {
+    int data;
+    Color color;
+    struct Node *left, *right, *parent;
+} Node;
+
+// Function to create a new node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->color = RED; // New nodes are initially red
+    newNode->left = newNode->right = newNode-> parent = NULL;
+    return newNode;
+}
+
 /* Do rotation under the four cases:
 case 0: Z = root
 case 1: Z.uncle = red -> recolor
@@ -14,6 +34,10 @@ Fix violations:
 */
 
 // Right Rotation
+void rotateRight(Node **root, Node* y) {
+    
+}
+
 // Left Rotation
 // Insertion
 /* Deletion 
